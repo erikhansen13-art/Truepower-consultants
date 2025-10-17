110271c4924246947d02e2142fa109bbde1f3fee
erikhansen13-art <erik.hansen13@icloud.com>
2025-10-17 12:14:47 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        brand: '#0B3954'
-      }
-    }
-  },
+  theme: { extend: { colors: { brand: '#0B3954' } } },
   plugins: []
 }
